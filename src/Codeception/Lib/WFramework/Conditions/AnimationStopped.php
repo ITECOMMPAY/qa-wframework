ae14ba8d4bb173965dd3d307ad407a025a3211c0
Egor Nelyubov <e.nelyubov@it.ecommpay.com>
2021-05-27 15:19:38 +0300

    Add AnimationStopped condition

---
+++ src/Codeception/Lib/WFramework/Conditions/AnimationStopped.php
@@ -0,0 +1,110 @@
+<?php
+
+
+namespace Codeception\Lib\WFramework\Conditions;
+
+
+use Codeception\Lib\WFramework\Logger\WLogger;
+use Codeception\Lib\WFramework\Operations\Execute\ExecuteScriptOnThis;
+use Codeception\Lib\WFramework\WebObjects\Base\WPageObject;
+
+class AnimationStopped extends AbstractCondition
+{
+    public function getName() : string
+    {
+        return "анимация закончилась?";
+    }
+
+    protected $timeout = null;
+
+    public function acceptWBlock($block) : bool
+    {
+        return $this->apply($block);
+    }
+
+    public function acceptWElement($element) : bool
+    {
+        return $this->apply($element);
+    }
+
+    protected function apply(WPageObject $pageObject) : bool
+    {
+        if ($this->timeout === null)
+        {
+            $this->timeout = $this->getExpirationTime($pageObject);
+        }
+
+        return time() >= $this->timeout;
+    }
+
+    protected function getExpirationTime(WPageObject $pageObject) : int
+    {
+        $timeout = time();
+
+        $animationTime = ((int) $pageObject->accept(new ExecuteScriptOnThis(static::SCRIPT_GET_TIMEOUT_MS))) / 1000;
+
+        if ($animationTime < 0)
+        {
+            $animationTime = 0;
+        }
+        elseif ($animationTime > 5 * 60)
+        {
+            $animationTime = 5 * 60;
+        }
+
+        WLogger::logDebug($this, "получили время анимации: $animationTime секунд");
+
+        return $timeout + $animationTime;
+    }
+
+    protected const SCRIPT_GET_TIMEOUT_MS = <<<EOF
+function* getSoleChildren(element) {
+    yield element;
+
+    while (element.children.length === 1) {
+        element = element.children[0];
+
+        yield element;
+    }
+}
+
+function findAnimatedElement(element) {
+    for (const childOrSelf of getSoleChildren(element)) {
+        if (childOrSelf.style.animationName.trim() !== '') {
+            return childOrSelf;
+        }
+    }
+
+    return null;
+}
+
+function getTimeInMs(str) {
+    if (typeof str !== 'string') {
+        return 0;
+    }
+
+    const regex = /(?<time>\d+)(?<units>s|ms)/;
+
+    let matches = regex.exec(str);
+
+    if (matches === null) {
+        return 0;
+    }
+
+    if (matches.groups.units === 's') {
+        return matches.groups.time * 1000;
+    }
+
+    return matches.groups.time;
+}
+
+let animatedElement = findAnimatedElement(arguments[0]);
+
+if (animatedElement === null) {
+    return 0;
+}
+
+return getTimeInMs(animatedElement.style.animation-delay) + getTimeInMs(animatedElement.style.animation-duration);
+EOF;
+
+}
